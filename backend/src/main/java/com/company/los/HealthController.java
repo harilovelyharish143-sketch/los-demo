5cf476b467a5996185ae6fd6e701bfc2ec4066bc
harilovelyharish143-sketch <harilovely.harish143@gmail.com>
2025-12-17 13:02:43 +0530

    Update package declaration in HealthController

--- backend/src/main/java/com/company/los/HealthController.java
+++ backend/src/main/java/com/company/los/HealthController.java
@@ -1,4 +1,4 @@
-package com.los.demo.controller;
+package com.company.los;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,3 +11,4 @@ public class HealthController {
         return "LOS Backend is running successfully";
     }
 }
+
